da0ebfe5f2aa3963c9812bdcd56bc4e8bb42122c
Amin Saffarnejad <vira1368@gmail.com>
2021-12-05 00:45:13 +0330

    Add Person model.

---
+++ AsyncStream/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace AsyncStream.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public int Age { get; set; }
+    }
+}
